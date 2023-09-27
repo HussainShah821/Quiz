95b315d198c8e4b53d957993ca0591befe9b8e64
Syed Hussain Shah <ssyedhussain821@gmail.com>
2023-09-27 12:49:31 +0500

    Tasks completed

--- js/index.js
+++ js/index.js
@@ -2,3 +2,24 @@
 function percentage(number, total_amount){
     return(number/total_amount)*100;
 }
+console.log(percentage(80,100))
+
+//Task 2
+const person=[
+	{
+		id: 1,
+		personName: "Fahad",
+		personAge: "23",
+		personIncome: "2000000"
+	},
+	{
+		id: 2,
+		personName: "Zoya",
+		personAge: "23",
+		personIncome: "3000000"
+	}
+];
+for(let i= 0; i<=Array.length; i++)
+console.log('person '+ person[i].id + ' name is '+ person[i].personName + " having salary "+ person[i].personIncome );
+
+console.log(person[0])
